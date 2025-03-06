a392b853e7d81cd7a83ab313cf4af480bc3a25b1
Ahmad-Al-Nabeli <97115953@st.deltion.nl>
2025-03-06 12:11:06 +0100

    ADD 404.php

---
+++ Site_met_navbar_en_een_db/404.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="nl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>404 Not Found</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <h1 style="color:red;">404 - Pagina niet gevonden</h1>
+    <p>De opgevraagde pagina bestaat niet.</p>
+    <a href="index.php?action=home">Terug naar home</a>
+</body>
+</html>
