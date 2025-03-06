23f006e531ae6bea1c9fa29a7f8ba42b0ff80272
Ahmad-Al-Nabeli <97115953@st.deltion.nl>
2025-03-06 12:09:56 +0100

    ADD nav.php

---
+++ Site_met_navbar_en_een_db/nav.php
@@ -0,0 +1,7 @@
+<nav>
+    <ul class="nav-list">
+        <li><a href="index.php?action=home">Home</a></li>
+        <li><a href="index.php?action=select">View Employees</a></li>
+        <li><a href="index.php?action=add">Add Employee</a></li>
+    </ul>
+</nav>
